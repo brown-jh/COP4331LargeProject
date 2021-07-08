01189de3b69430f5dee2ce55cf22b2613012f921
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-08 19:56:15 -0400

    Improving the formatting of the event box

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -9,7 +9,7 @@ const TestPage = () =>{
                 group={"Women's Tennis Club of Orlando"}
                 time={"Sunday, April 23rd, 2:00 PM"}
                 place={"Big Win Gym, 4913 Greensteel Drive, Orlando, FL, 32828"}
-                tags={["Sports", "Tennis", "Test Tag 1", "Test Tag 2", "Test Tag 3"]} />
+                tags={["Sports", "Tennis", "Test Tag 1", "Test Tag 2", "Test Tag 3", "Very very long tag to check wrapping"]} />
         </div>
     );
 };
