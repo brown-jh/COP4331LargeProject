26ef0723641b72292c9df2fb7cef9bfe201bef99
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-08 18:09:11 -0400

    Another attempt to make the divs shrink or expand to fit the text

--- frontend/src/components/TagMarker.js
+++ frontend/src/components/TagMarker.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TagMarker(props)
 {
     return(
-        <span style={{fontSize: "15px", borderRadius:"30px", borderStyle: "solid", borderColor: "#000000"}}>
+        <span style={{display: "inline-block", fontSize: "15px", borderRadius:"30px", borderStyle: "solid", borderColor: "#000000"}}>
             <p>{props.txt}</p>
         </span>
     );
