07803643ab1151aa9864add5ecd3c1d5b0137c59
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-08 17:58:38 -0400

    More styling on the tag markers to make them easier to see

--- frontend/src/components/TagMarker.js
+++ frontend/src/components/TagMarker.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TagMarker(props)
 {
     return(
-        <div style={{fontSize: "15px", borderRadius:"30px"}}>
+        <div style={{fontSize: "15px", borderRadius:"30px", borderStyle: "solid", borderColor: "#000000"}}>
             <p>{props.txt}</p>
         </div>
     );
