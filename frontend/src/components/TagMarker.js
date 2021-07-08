461c7fce67ec48a1aba11edbdd95074db2275505
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-08 18:38:50 -0400

    Tag Marker CSS continued
    Looks like I had the color and backgroundColor mixed up; let's see what this does.

--- frontend/src/components/TagMarker.js
+++ frontend/src/components/TagMarker.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TagMarker(props)
 {
     return(
-        <span style={{display: "inline", fontSize: "15px", color: "#CCCCCC", border: "1px solid black", borderRadius:"15px"}}>
+        <span style={{display: "inline", fontSize: "15px", backgroundColor: "#CCCCCC", border: "1px solid black", borderRadius:"15px"}}>
             <p>{props.txt}</p>
         </span>
     );
