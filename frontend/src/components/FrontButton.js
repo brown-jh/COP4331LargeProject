0324df3c569d1845ce09f33135623593caae3e83
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-09 20:09:34 -0400

    More format tweaking on the home page

--- frontend/src/components/FrontButton.js
+++ frontend/src/components/FrontButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FrontButton(props)
 {
     return(
-        <div style={{width: "40%", margin:"3%", float: "left", backgroundColor: "#89CFF0", 
+        <div style={{width: "40%", margin:"4%", float: "left", backgroundColor: "#89CFF0", 
             border: "2px solid orange", borderRadius: "10px"}}>
 
             <p style={{textAlign:"center"}} onClick={props.clickAct}>{props.txt}</p>
