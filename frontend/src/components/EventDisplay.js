2d712b4985bb9c2e6925a5bcfd4a6082772dbe51
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-07-12 22:04:19 -0400

    Started the page that displays events; lots of tech missing
    This should be the start of the layout for the page that displays events. A lot of tech is missing (the APIs, the checkbox, etc, but in particular I want the page to be able to pull the event ID from the URL (like get2gather.com/events/1648264739) and update the page based on that. This should at least be a start.

---
+++ frontend/src/components/EventDisplay.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+
+function EventDisplay()
+{
+    var userId;
+
+    const[eventTitle, setEventTitle] = useState('');
+    const[eventDesc, setEventDesc] = useState('');
+    const[eventHost, setEventHost] = useState('');
+    const[attendeeList, setAttendeeList] = useState('');
+    const[eventGroup, setEventGroup] = useState('');
+    const[eventTime, setEventTime] = useState('');
+    const[eventLocation, setEventLocation] = useState('');
+
+    useEffect(() => {
+        //Here, we would normally pull the event ID from the URL, get the event via API, 
+        // and use it to get the event's data, but we'll use dummy data for now.
+
+        thisEvent={
+            name: "Smash Tournament for NerdKnighteria",
+            host: "Hannah Wrigley",
+            description: "NerdKnighteria is holding a Smash 4 tournament at the Student Union at 3 on Monday. Bring snacks and controllers, cash prizes to be awarded up to 50$.",
+            attendees: ["John Smith", "Alyx Reckahn", "Hannah Wrigley"],
+            group: "NerdKnighteria of UCF",
+            time: "July 31 2021, 3:00 PM",
+            place: "Student Union, University of Central Florida, Orlando"
+        };
+
+        userId = "Eddie Johnson";
+
+        setEventTitle(thisEvent.name);
+        setEventDesc(thisEvent.description);
+        setEventHost(thisEvent.host);
+        setAttendeeList(thisEvent.attendees.map((eventMember) => <div><p>{eventMember}</p><br/></div>));
+        setEventGroup(thisEvent.group);
+        setEventTime(thisEvent.time);
+        setEventLocation(thisEvent.place);
+    });
+
+    return(
+        <div>
+            <h1>{eventTitle}</h1>
+            <p>Hosted by {eventHost}</p>
+
+            {/* Display the edit button to hosts and the attending checkbox to other users.*/}
+            {
+                eventHost == userId.value ?
+                <input type="button" onClick={alert("Redirect to edit page")}>Edit/Cancel Event</input>
+                :
+                <div>
+                    <input type="checkbox" id="attendingCheck"/>
+                    <label for="attendingCheck">Attending</label>
+                </div>
+            }
+
+            <div style={{width:"40%", marginLeft:"9%", float:"left"}}>
+                <p>TODO: Add photos?</p>
+                <h2>Decsription</h2>
+                <p>{eventDesc}</p>
+
+                <p>Attendees:</p>
+                {attendeeList}
+
+            </div>
+            <div style={{width:"40%", marginLeft:"9%", float:"left"}}>
+                <p>{eventGroup}</p>
+                <p>{eventTime}</p>
+                <p>{eventLocation}</p>
+            </div>
+
+            <br/>
+            <div>
+                <p>TODO: Add comments?</p>
+            </div>
+
+        </div>
+        
+    )
+}
+
+export default EventDisplay;
