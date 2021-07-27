f85cdf8d002560567cf38c2743aeefa2844064e3
Jayden Sipe <jayden.sipe@gmail.com>
2021-07-27 01:55:02 -0400

    Try changing value

--- frontend/src/components/VerifyAccount.js
+++ frontend/src/components/VerifyAccount.js
@@ -10,15 +10,13 @@ function VerifyAccount(props){
         
         // Maybe change .value
         var obj = props.verifyId;        
-        var js = JSON.stringify(obj); 
-        console.log("FROM VERIFYACCOUNT.JS:" + js + " " + obj)
         var res;
         const fetchdata = async () => 
             {
             try        
                 {            
                     const response = await fetch(bp.buildPath('api/verifyaccount'),            
-                        {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+                        {method:'POST',body:obj,headers:{'Content-Type': 'application/json'}});
                     var txt = await response.text();   
                     res = JSON.parse(txt);   
                     if( res.error.length > 0 )            
