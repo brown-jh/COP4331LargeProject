dc67d6dab9ece72b32fce524ccd73193e9bd0fd1
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-07-27 00:34:23 -0400

    Hosted events page

--- api.js
+++ api.js
@@ -484,7 +484,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Events').find({ "EventHosts": [_search] }).toArray();
+        const results = await db.collection('Events').find({ "EventHosts": _search }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
@@ -527,7 +527,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Events').find({ "EventAttendees": [_search] }).toArray();
+        const results = await db.collection('Events').find({ "EventAttendees": _search }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
@@ -570,7 +570,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Groups').find({ "GroupSubscribers": [_search] }).toArray();
+        const results = await db.collection('Groups').find({ "GroupSubscribers": _search }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
--- frontend/src/components/HostedEvents.js
+++ frontend/src/components/HostedEvents.js
@@ -5,55 +5,58 @@ import EventBox from '../components/EventBox';
 function HostedEvents()
 {
 
+    var bp = require('../components/Path.js');
+
+    var storage = require('../tokenStorage.js');
+    const jwt = require("jsonwebtoken");
+
+    var _ud = localStorage.getItem('user_data');    
+    var ud = JSON.parse(_ud);    
+    var userId = ud.id;
+
     const [hostedEvents, setHostedEvents] = useState('');
 
     useEffect(() => {
-        // TODO: Here we would find the user's hosted events via API and put them in here.
-        var dummyHostedEvents=[
+        
+        var tok = storage.retrieveToken();
+        var obj = {search:userId,jwtToken:tok};
+        var js = JSON.stringify(obj);
+
+        async function fetchData(){
+            try
             {
-                id: "123",
-                title: "Sunday Practice for Orlando Tennis Club",
-                group: "Women's Tennis Club of Orlando",
-                time: "April 23rd, 2021 2:00 PM",
-                place: "Big Win Gym, 4913 Greensteel Drive, Orlando, FL, 32828"
-            },
-            {
-                id: "69420",
-                title: "Weekly D&D Night, Newcomers Welcome",
-                group: "",
-                time: "April 21st, 2021 8:00 PM",
-                place: "2123 Rose Lane, Orlando, FL, 32819"
-            },
-            {
-                id: "11111",
-                title: "JavaScript Workshop",
-                group: "Programming Club of UCF",
-                time: "April 26th, 2021 3:00 PM",
-                place: "Online"
-            },
-            {
-                id: "492307816",
-                title: "Super Smash Bros Tournament - Cash Prizes",
-                group: "NerdKnighteria of UCF",
-                time: "May 12th, 2021 5:00 PM",
-                place: "Online"
-            },
-            {
-                id: "9630",
-                title: "April Meeting of Jacaranda Book Club",
-                group: "Jacaranda Book Club",
-                time: "April 6th, 2021 1:00PM",
-                place: "4143 Woodmere Park Blvd, Venice, FL 34293"
+                const response = await fetch(bp.buildPath('api/searchgroupadmins'),
+                    {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+                var txt = await response.text();
+                var res = JSON.parse(txt);
+
+  
+                setHostedEvents(res.results.map((eventData) => (
+                    <EventBox 
+                        imageURL={eventData.ImageURL}
+                        title={eventData.EventName}
+                        group={eventData.EventDescription}
+                        // Ensures dates are in: Month Day, Year Time format
+                        time={new Date(eventData.EventTime).toLocaleString('en-us', {year: 'numeric', month: 'long', day: '2-digit'}).
+                        replace(/(\d+)\/(\d+)\/(\d+)/, '$1-$2-$3') + " " + new Date(eventData.EventTime).toLocaleTimeString()}
+                        place={eventData.EventLocation}/>)));
+                        
+                    var retTok = res.jwtToken;
+                   storage.storeToken( retTok );
+                  return;
+
             }
-        ]
-        setHostedEvents(dummyHostedEvents.map((eventData) => (
-            <EventBox title={eventData.title}
-                group={eventData.group}
-                eventId={eventData.id}
-                // Ensures dates are in: Month Day, Year Time format
-                time={new Date(eventData.time).toLocaleString('en-us', {year: 'numeric', month: 'long', day: '2-digit'}).
-                replace(/(\d+)\/(\d+)\/(\d+)/, '$1-$2-$3') + " " + new Date(eventData.time).toLocaleTimeString()}
-                place={eventData.place}/>)));
+            catch(e)
+            {
+                
+                return;
+            }
+        };
+
+        fetchData();
+
+        
     });
 
     return (
