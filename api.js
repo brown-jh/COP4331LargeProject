70a760db7555ebcd8413c0b7aa8ec38565dae79b
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-07-26 22:36:19 -0400

    Trying to fix search
    change searchevent query

--- api.js
+++ api.js
@@ -614,7 +614,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Events').find({$or: [ { "EventName": {$regex:_search+'.*i', $options:'i'} }, {"EventDescription": {$regex:_search+'.*i', $options:'i'} } ] }).toArray();
+        const results = await db.collection('Events').find({$or: [ { "EventName": {$regex:'/^'+_search+'/i'} }, {"EventDescription": {$regex:'/^'+_search+'/i'} } ] }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
