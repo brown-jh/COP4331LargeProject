68125cd2a6adb8d7475367e813c340da806b6ce5
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-07-27 20:58:49 -0400

    Update EventDisplay.js

--- frontend/src/components/EventDisplay.js
+++ frontend/src/components/EventDisplay.js
@@ -49,22 +49,17 @@ function EventDisplay(props)
                     var txt = await response.text();   
                     alert(txt);
                     res = JSON.parse(txt);    
-                    //var _res = JSON.parse(res.results);
-
-                    alert(res.results[0]);
-                    alert(res.results[0].EventName);
-
 
                         setEventTitle(res.results[0].EventName + "\nEvent ID: " + props.eventId); //To test the parameter pass-in.
-                        //setEventDesc(res.results[0].EventDescription);
-                        //setEventHost(res.results[0].EventHosts);
+                        setEventDesc(res.results[0].EventDescription);
+                        setEventHost(res.results[0].EventHosts);
                         //setAttendeeList(<div><p>{makeUsernameList(res.results[0].EventAttendees)}</p></div>);
                         //attendeeVar = res.results[0].EventAttendees; //So we can access the attendees outside of useEffect.
-                        //setEventGroup(res.results[0].GroupID);
-                        //setEventTime(res.results[0].EventTime);
-                        //setEventLocation(res.results[0].EventLocation);
+                        setEventGroup(res.results[0].GroupID);
+                        setEventTime(res.results[0].EventTime);
+                        setEventLocation(res.results[0].EventLocation);
                         //setEventComments(res.results[0].EventComments);
-                        //setEventImage(res.results[0].ImageURL); //Dummy data, fix with rest of API call.
+                        setEventImage(res.results[0].ImageURL); //Dummy data, fix with rest of API call.
 
                         // Flip the status of the join/leave button to Leave if the user is in the list of attendees.
                         if (attendeeVar.filter(user => user.id == userId).length != 0)
