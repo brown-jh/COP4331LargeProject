1cd04cb08352b0e1f92e95de0ee02024ceab2f11
Joseph Brown <brown_jh@knights.ucf.edu>
2021-07-28 04:42:29 -0400

    fixing search params for groups

--- api.js
+++ api.js
@@ -560,7 +560,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Groups').find({ "GroupAdmins": _search }).toArray();
+        const results = await db.collection('Groups').find({ "GroupAdmins.Id": _search }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
@@ -891,7 +891,7 @@ exports.setApp = function (app, client)
         var _search = search.trim();  
 
         const db = client.db();  
-        const results = await db.collection('Groups').find({ "GroupSubscribers": _search }).toArray();
+        const results = await db.collection('Groups').find({ "GroupSubscribers.Id": _search }).toArray();
 
         var _ret = [];  
         for( var i=0; i<results.length; i++ )  
